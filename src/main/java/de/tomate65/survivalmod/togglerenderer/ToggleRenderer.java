de41a43e0548fdf74a97f4357b93bc50d02b2670
1Tomate65 <>
2025-03-19 20:09:53 +0100

    add foundation für toggle command

---
+++ src/main/java/de/tomate65/survivalmod/togglerenderer/ToggleRenderer.java
@@ -0,0 +1,11 @@
+package de.tomate65.survivalmod.togglerenderer;
+
+public class ToggleRenderer {
+
+    //Here is the class for the toggle command that can be rendered ingame
+    //Idea/ Concept
+    //with /toggle (Item_ID) (location) you should be able to display your statistics in the >Scoreboard / Title / actionbar / bossbar<,
+    //how many of them you have already mined.
+    //Man It should preferably be saved in a separate file.
+
+}
